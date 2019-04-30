67de52047781c7469bb30301da9c5452f33fceb8
Andres Flores <floresc.andres@gmail.com>
2019-04-29 20:51:59 -0400

    Added more asserts to test 4

--- LotteryService.Tests/test4.cs
+++ LotteryService.Tests/test4.cs
@@ -11,7 +11,8 @@ public class LotteryServiceTenNumSortedTest
         int[] numArr = new int[] { 5, 1, 9, 10, 8, 7, 5, 500, 6970, 3};
         obj.SortNumbers(numArr);
         var results = obj.SortNumbers(numArr);
-        int[] numArr2 = new int[] { 1, 5, 9, 3, 6970, 5, 10, 8, 7, 500}; 
+        int[] numArr2 = new int[] { 1, 3, 5, 5, 7, 8, 9, 10, 500, 6970};
         Assert.AreEqual(numArr2[0], results[0]);
+        Assert.AreEqual(numArr2[5], results[5]);
     }
 }
